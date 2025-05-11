fa7b9f8da7419ce9cf53d656808603b3e1ee8282
d4m14n-gh <damiantrowski.c@gmail.com>
2025-05-11 11:13:50 +0200

    Clean repo files

--- src/start.py
+++ src/start.py
@@ -2,6 +2,7 @@
 
 import os
 from http.server import HTTPServer, SimpleHTTPRequestHandler
+import argparse
 
 
 class NoCacheHandler(SimpleHTTPRequestHandler):
@@ -11,17 +12,25 @@ class NoCacheHandler(SimpleHTTPRequestHandler):
         self.send_header("Expires", "0")
         super().end_headers()
 
+def main():
+    parser = argparse.ArgumentParser(description="Start a simple HTTP server.")
+    parser.add_argument('-p', '--port', type=int, default=8000, help='Port to run the server on (default: 8000)')
+    parser.add_argument('-a', '--address', type=str, default='localhost', help='Host address (default: localhost)')
+    args = parser.parse_args()
+    serve(args.address, args.port)
 
-serve_dir = os.path.dirname( os.path.abspath(__file__) )
-os.chdir(serve_dir)
 
-host = "0.0.0.0"
-port = 8000
-server_address = (host, port)
+def serve(host, port):
+    serve_dir = os.path.dirname( os.path.abspath(__file__) )
+    os.chdir(serve_dir)
+    server_address = (host, port)
+    print(f"Server working on http://{host}:{port} -> {serve_dir}")
+    httpd = HTTPServer(server_address, NoCacheHandler)
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print(" Closing server...")
 
-print(f"Server working on http://{host}:{port} -> {serve_dir}")
-httpd = HTTPServer(server_address, NoCacheHandler)
-try:
-    httpd.serve_forever()
-except KeyboardInterrupt:
-    print(" Closing server...")
+
+if __name__ == "__main__":
+    main()
